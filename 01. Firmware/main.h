5a73e23eaf560842476241fec69434756459f121
minh-luong <lvminh97@gmail.com>
2025-06-22 22:16:04 +0700

    - Update simulation   : decrease the OSC frequency to 4MHz   : remove unused pin - Firmware   : define GPIO pins   : measure the motor's speed by encoder and show on the LCD
    Signed-off-by: minh-luong <lvminh97@gmail.com>

--- 01. Firmware/main.h
+++ 01. Firmware/main.h
@@ -1,4 +1,32 @@
 #include <16F877A.h>
+
 #device ADC=10
-#use delay(crystal=20000000)
+#fuses NOWDT, NOBROWNOUT, NOLVP, HS 
+#use delay(crystal=4000000)
+
+// LCD1602
+#define LCD_ENABLE_PIN  PIN_D2
+#define LCD_RS_PIN      PIN_D0
+#define LCD_RW_PIN      PIN_D1
+#define LCD_DATA4       PIN_D4
+#define LCD_DATA5       PIN_D5
+#define LCD_DATA6       PIN_D6
+#define LCD_DATA7       PIN_D7
+#include <lcd.c>
+
+// Buttons
+#define BUTTON_START       PIN_B3
+#define BUTTON_STOP        PIN_B4
+#define BUTTON_ADC_MODE    PIN_B5
+#define BUTTON_FORW        PIN_B6  
+#define BUTTON_REVR        PIN_B7
+
+// Encoder
+#define ENCODER         PIN_B0 
+
+// Motor
+#define MOTOR_DIR_1     PIN_C6
+#define MOTOR_DIR_2     PIN_C7
+
+#define PPR    24
 
